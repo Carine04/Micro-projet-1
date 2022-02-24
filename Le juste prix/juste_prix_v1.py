eec11af2b8d50346c9f56ba2ac6e78181d285507
Carine <muhecarine@gmail.com>
2022-02-24 21:45:48 +0100

    ajuster le code avec le nombre de  tentation = 10

--- Le juste prix/juste_prix_v1.py
+++ Le juste prix/juste_prix_v1.py
@@ -1,2 +1,15 @@
-print("hello")
-print("bonjour")
+from random import randint
+print("Bienvenue au juste prix")
+juste_prix = randint(1, 100)
+prix_proposé = int(input("Entrer un prix : "))
+
+while juste_prix != prix_proposé:
+    if  prix_proposé > juste_prix:
+        print("c'est moins")
+        prix_proposé = int(input("entrer un prix: "))
+    else:
+        print("c'est plus")
+        prix_proposé = int(input("entrer un prix: " ))
+if juste_prix == prix_proposé:
+    print("Félicitation! vous avez gagné")
+    exit()
